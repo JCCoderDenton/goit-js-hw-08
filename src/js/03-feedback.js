c65eda4377325c9274b77ba4fe6ab077f937c148
JCCoderDenton <denys_m99@ukr.net>
2023-12-15 19:34:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedState) {
+  emailInput.value = savedState.email;
+  messageInput.value = savedState.message;
+}
+
+const saveFormState = throttle(() => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}, 500);
+
+feedbackForm.addEventListener('input', saveFormState);
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+});
