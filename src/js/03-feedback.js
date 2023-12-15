9e9ea9e33ae9e48706d5d472c04b33b8896bd960
JCCoderDenton <denys_m99@ukr.net>
2023-12-15 19:44:04 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,41 @@
 import throttle from 'lodash.throttle';
 
+/*
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
-const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');*/
+
+let form = document.forms[0];
+let formEmail = form.elements.email;
+let formMassage = form.elements.message;
+
 
 const STORAGE_KEY = 'feedback-form-state';
 
 const savedState = JSON.parse(localStorage.getItem(STORAGE_KEY));
 if (savedState) {
-  emailInput.value = savedState.email;
-  messageInput.value = savedState.message;
+  formEmail = savedState.email;
+  formMassage = savedState.message;
 }
 
 const saveFormState = throttle(() => {
   const state = {
-    email: emailInput.value,
-    message: messageInput.value,
+    email: formEmail,
+    message: formMassage,
   };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
 }, 500);
 
-feedbackForm.addEventListener('input', saveFormState);
+form.addEventListener('input', saveFormState);
 
-feedbackForm.addEventListener('submit', event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const state = {
-    email: emailInput.value,
-    message: messageInput.value,
+    email: formEmail,
+    message: formMassage,
   };
   console.log(state);
   localStorage.removeItem(STORAGE_KEY);
-  emailInput.value = '';
-  messageInput.value = '';
+  formEmail = '';
+  formMassage = '';
 });
